8eae23d15b0f93cd74cca433134649ac42e4ca63
NitinSangwan-584 <nitinsangwan363@gmail.com>
2024-12-31 14:26:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("layer").style.display = "block";
+    document.getElementById("mySidebar").style.width = "286px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("layer").style.display = "none";
+}
